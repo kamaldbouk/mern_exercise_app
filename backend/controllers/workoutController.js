dda3b5bca7789970fcf94885b7e8dc60ad19f5a2
kamaldbouk <kamaldbouk13@gmail.com>
2025-01-09 12:44:45 +0200

    Create Workout DONE

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,32 @@
+const Workout = require('../models/workoutModel')
+
+// get all workouts
+
+
+// get single workout
+
+
+// create new workout
+const createWorkout = async (req, res) => {
+    const {title, load, reps} = req.body
+
+    // add doc to db
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout);
+    }
+    catch (error) {
+        res.status(400).json({error: error.msg});
+    }
+}
+
+// delete a workout
+
+
+// update a workout
+
+
+
+module.exports = {
+    createWorkout
+}
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const Workout = require('../models/workoutModel')
-const router = express.Router();
+const router = express.Router()
+const { 
+    createWorkout
+} = require('../controllers/workoutController')
 
 // get all workouts
 router.get('/', (req, res) => {
@@ -13,17 +16,7 @@ router.get('/:id', (req, res) => {
 }) 
 
 // post new workout
-router.post('/', async (req,res) => {
-
-    const {title, load, reps} = req.body
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout);
-    }
-    catch (error) {
-        res.status(400).json({error: error.msg});
-    }
-})
+router.post('/', createWorkout);
 
 // delete a workout
 router.delete('/:id', (req,res) => {
